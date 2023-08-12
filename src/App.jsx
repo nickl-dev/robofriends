751117155200c08405b41dfab6e915a2c17c45c9
Nick Lal <60986212+nickl-dev@users.noreply.github.com>
2023-08-12 09:18:35 -0600

    -Refactored the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,18 @@ function App () {
   const [robots, setRobots] = useState([]);
   const [searchInput, setSearchInput] = useState('');
 
-  async useEffect (() => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      const data = await response.json();
-      setRobots(data);
-    } catch (error) {
-      throw error;
+  useEffect (() => {
+    const fetchRobots = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json();
+        setRobots(data);
+      } catch (error) {
+        throw error;
+      }
     }
+
+    fetchRobots();
   }, [])
 
   const onSearchChange = (event) => {
