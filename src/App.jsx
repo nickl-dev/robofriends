bd6b0b2d5746e7301b9db779dc5beb34795f1bba
Nick Lal <60986212+nickl-dev@users.noreply.github.com>
2023-08-24 10:52:52 -0600

    - Remove fetchRobots function

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,11 @@ function App () {
   const [robots, setRobots] = useState([]);
   const [searchInput, setSearchInput] = useState('');
 
-  const fetchRobots = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  useEffect (async () => {
-    setRobots(await fetchRobots());
+  useEffect (() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setRobots(data))
+      .catch((error) => { throw error })
   }, [])
 
   const onSearchChange = (event) => {
